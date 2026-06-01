652c44eec78cc4c849b1f3b49def883a43337fcf
delciacosta9 <130351561+delciacosta9@users.noreply.github.com>
2026-06-01 13:44:15 +0100

    Arquivo editado
    Arquivo editado, consultas actualizadas

--- Consultas/temporadas_nba.sql
+++ Consultas/temporadas_nba.sql
@@ -1,6 +1,5 @@
-
-create database nba;
-create table temporadas(
+CREATE DATABASE IF NOT EXISTS nba;
+CREATE TABLE IF NOT EXISTS temporadas(
 id smallint,
 nome_jogador varchar(255),
 abreviacao_team varchar(255),
@@ -9,9 +8,9 @@ altura float,
 peso float,
 universidade varchar(255),
 pais varchar(255),
-ano_draft smallint,
-ronda_draft tinyint,
-num_draft tinyint,
+ano_draft varchar(255),
+ronda_draft varchar(255),
+num_draft varchar(255),
 jogos tinyint,
 pontos float,
 ressaltos float,
@@ -25,13 +24,14 @@ percentual_assistencias float,
 temporada varchar(255)
 ); 
 
+
 /* 
 Procedimento de Limpeza de Dados
 Os seguintes problemas estão sendo tratados:
 1. Remover espaços em branco no início e no fim de todas as colunas de texto/VARCHAR
-2. As colunas ano_draft, ronda_draft e num_draft eram originalmente strings porque continham o valor "Undrafted".
+2. As colunas draft_year, draft_round e draft_number eram originalmente strings porque continham o valor "Undrafted".
    O plano é substituir todos os valores "Undrafted" por NULL e converter as colunas afetadas para o tipo INT
-3. A coluna temporada veio no formato "1996-97"
+3. A coluna season veio no formato "1996-97"
    O plano é extrair a temporada inicial "1996" de acordo com as convenções da NBA e converter para INT
 4. Modificar as colunas para refletir as mudanças nos tipos de dados
 */
@@ -59,11 +59,27 @@ MODIFY COLUMN ronda_draft INT,
 MODIFY COLUMN num_draft INT,
 MODIFY COLUMN temporada YEAR;
 
--- Verificando as modificações feitas
+-- Verificando as modificacoes feitas
 DESCRIBE temporadas;
 
+-- Verificando duplicados
+SELECT
+	nome_jogador, count(*)
+FROM
+	temporadas
+Group by nome_jogador, temporada, idade, abreviacao_team, universidade
+HAVING count(*) > 1;
+
+-- Nenhum registro duplicado foi encontrado.
 
 -- Verificando valores irrealistas
+SELECT
+	*
+FROM
+	temporadas
+WHERE
+	jogos < 0 OR pontos < 0 OR ressaltos < 0 OR assistencias < 0 OR classificacao_liquida < -30 OR classificacao_liquida > 30;
+
 -- Criando uma tabela duplicada
 DROP TABLE IF EXISTS temporadas_copia;
 CREATE TABLE temporadas_copia AS (
@@ -100,14 +116,14 @@ SELECT
 	COUNT(id) AS num_registros
 FROM
 	temporadas;
--- Existem 10.422 registros no conjunto de dados
+-- Existem 12.844 registros no conjunto de dados
 
 -- Quantos jogadores únicos existem no conjunto de dados?
 SELECT 
 	COUNT(DISTINCT nome_jogador) AS jogadores_unicos
 FROM 
 	temporadas;
--- Existem 1.757 jogadores únicos
+-- Existem 2.551 jogadores únicos
 
 -- Quantas equipes únicas existem nos dados
 SELECT 
@@ -128,7 +144,7 @@ SELECT
 	COUNT(DISTINCT pais) AS pais
 FROM 
 	temporadas;
--- Existem 66 países diferentes representados na NBA
+-- Existem 82 países diferentes representados na NBA
 
 -- Os 5 principais países
 SELECT 
@@ -138,7 +154,8 @@ FROM
 GROUP BY 
 	pais
 ORDER BY pais DESC LIMIT 5;
--- Percentagem de cada pais
+
+-- percentagem de cada pais
 SELECT 
     pais,
     COUNT(*) AS quantidade,
@@ -149,14 +166,14 @@ SELECT
 FROM temporadas
 GROUP BY pais
 ORDER BY percentagem DESC;
--- A maioria dos jogadores da NBA, 83,9%, é dos EUA.
+-- A maioria dos jogadores da NBA, 83,47%, é dos EUA.
 
 -- Quantas universidades produzem jogadores da NBA
 SELECT 
 	COUNT(DISTINCT universidade) AS qtd_universidade
 FROM 
 	temporadas;
--- 251 universidades produzem jogadores da NBA
+-- 354 universidades produzem jogadores da NBA
 
 -- Quais são as 10 universidades que mais produzem jogadores
 SELECT 
@@ -165,10 +182,10 @@ FROM
 	temporadas
 GROUP BY universidade
 ORDER BY qtd_universidade DESC limit 10;
--- Descobriu-se que 1611 jogadores não vieram de nenhuma universidade. Kentucky, Duke e North Carolina
+-- Descobriu-se que 1854 jogadores não vieram de nenhuma universidade. Kentucky, Duke e North Carolina
 -- lideraram como as universidades que mais produziram jogadores da NBA
 
--- Verificando os valores máximos e mínimos de estatísticas como jogos disputados, pontos, rebotes, assistências e classificação liquida para possíveis outliers
+-- Verificar os valores máximos e mínimos de estatísticas como jogos disputados, pontos, rebotes, assistências e classificação liquida para possíveis outliers
 SELECT 
 	max(jogos), min(jogos)
 FROM temporadas;
@@ -184,8 +201,8 @@ FROM temporadas;
 SELECT 
 	max(classificacao_liquida), min(classificacao_liquida)
 FROM temporadas;
--- Parece que a classificação liquida possuem valores extremamente fora do padrão; máximo: 114.3, mínimo: -250
--- Isso precisa de investigação adicional, já que classificações liquidas normalmente variam de -30 a +30
+-- Parece que a classificação liquida possuem valores extremamente fora do padrão; máximo: 300, mínimo: -250
+-- Isso precisa de investigação adicional, já que classificação liquida normalmente variam de -30 a +30
 
 -- Verificar os valores máximos e mínimos das estatísticas avançadas
 SELECT 
@@ -201,34 +218,34 @@ SELECT
 	max(percentual_ressaltos_ofensivos), min(percentual_ressaltos_ofensivos)
 FROM temporadas;
 -- As estatísticas avançadas em porcentagem variam entre 0 e 1
--- Exceto o percentual_arremesso_verdadeiro, que apresentou um máximo de 1,5, o que é impossível. Precisa de investigação e correção.
+-- Exceto o percentual arremesso verdadeiro, que apresentou um máximo de 1,5, o que é impossível. Precisa de investigação e correção.
 
--- Verificando os outliers em classificacao_liquida
+-- Verificando os outliers em classificação liquida
 SELECT 'MAXIMO' AS tipo,
 	nome_jogador
 FROM temporadas
-where classificacao_liquida = (SELECT MAX(classificacao_liquida) FROM temporadas)
+WHERE classificacao_liquida = (SELECT MAX(classificacao_liquida) FROM temporadas)
 
 UNION ALL
 
 SELECT 'MINIMO' AS tipo,
 	nome_jogador
 FROM temporadas
-where classificacao_liquida = (SELECT MIN(classificacao_liquida) FROM temporadas)
+WHERE classificacao_liquida = (SELECT MIN(classificacao_liquida) FROM temporadas)
 ;
 
--- Verificando os outliers em arremesso_verdadeiro
+-- Verificando os outliers em arremesso verdadeiro
 SELECT 'MAXIMO' AS tipo,
 	nome_jogador
 FROM temporadas
-where percentual_arremesso_verdadeiro = (SELECT MAX(percentual_arremesso_verdadeiro) FROM temporadas)
+WHERE percentual_arremesso_verdadeiro = (SELECT MAX(percentual_arremesso_verdadeiro) FROM temporadas)
 
-union all
+UNION ALL
 
 SELECT 'MINIMO' AS tipo,
 	nome_jogador
 FROM temporadas
-where percentual_arremesso_verdadeiro = (SELECT MIN(percentual_arremesso_verdadeiro) FROM temporadas)
+WHERE percentual_arremesso_verdadeiro = (SELECT MIN(percentual_arremesso_verdadeiro) FROM temporadas)
 ;
 
 /*
@@ -274,9 +291,9 @@ SET
     temp.percentual_arremesso_verdadeiro = 
 		CASE WHEN temp.percentual_arremesso_verdadeiro > 1 THEN outliers_jogadores_media.media_arremesso_verdadeiro
         ELSE temp.percentual_arremesso_verdadeiro END;
--- Apgar TEMPORARY TABLE outliers_jogadores;
+-- DROP TEMPORARY TABLE outliers_jogadores;
 DROP TEMPORARY TABLE outliers_jogadores;
--- Apagar TEMPORARY TABLE outliers_jogadores_media;
+-- DROP TEMPORARY TABLE outliers_jogadores_media;
 DROP TEMPORARY TABLE outliers_jogadores_media;
 -- Verificar se as alterações funcionaram
 SELECT DISTINCT
@@ -295,7 +312,7 @@ WHERE
 ```
 Classificar jogadores em cada temporada por pontos, rebotes e assistências por jogo.
 
-Comparar estatísticas de eficiência (Arremesso verdadeiro% vs uso%) — jogadores de alto volume sacrificam eficiência?
+Comparar estatísticas de eficiência (arremesso verdadeiro% vs uso%) — jogadores de alto volume sacrificam eficiência?
 
 Identificar os jogadores que mais evoluíram entre temporadas (maior aumento em pontos/rebotes/assistências).
 ```
@@ -319,7 +336,6 @@ SELECT
     ressaltos  
 FROM 
 	temporadas;
-
 -- Ranking por assistências por jogo
 SELECT
 	temporada,
@@ -361,8 +377,8 @@ WHERE
 	diferenca_pontos > 0
 ORDER BY diferenca_pontos DESC
 LIMIT 10;
-/*  MarShon Brooks em 2017 melhorou sua média de pontos em 15,6. Ele foi seguido de perto por Paul George,
-que em 2015 melhorou sua pontuação em 14.3, assim como Khyri Thomas.
+/*  MarShon Brooks em 2017 melhorou sua média de pontos em 15,6. Ele foi seguido de perto por Louis King,
+que em 2022 melhorou sua pontuação em 15.5.
 JaKarr Sampson em 2018 melhorou sua pontuação em 15,3.
 */
 
@@ -415,7 +431,7 @@ ORDER BY diferenca_assistencia DESC
 LIMIT 10;
 
 /* Skylar Mays em 2022 melhorou suas assistências em 7,7
-Kendall Marshall em 2013 melhorou em 5,8
+Derrick Walton em 2021 melhorou em 6
 */
 
 /*
@@ -459,11 +475,11 @@ GROUP BY
 	era;
 /*
 Apesar da redução física ao longo das eras, os índices de desempenho, como média de pontos, aumentaram
-de 8,432 nos anos 1990 para 10,142 nos anos 2020.
-A média de assistências por jogo também aumentou de 1,869 nos anos 1990 para 2,263 nos anos 2020.
-A media de classificacao_liquida também apresentou melhoria, saindo de -1,39 nos anos 2000 para -1,233 nos anos 2020.
-A media de percentual de uso apresentou uma queda constante de 0,192 nos anos 1990 para 0,186 nos anos 2020.
-A media de percentual de arremesso verdadeiro também aumentou gradualmente de 0,49 nos anos 1990 para 0,55 nos anos 2020.
+de 7,83 nos anos 1990 para 8,747 nos anos 2020.
+A média de assistências por jogo também aumentou de 1,787 nos anos 1990 para 1,971 nos anos 2020.
+A media de classificacao_liquida também apresentou melhoria, saindo de -1,634 nos anos 2000 para -1,575 nos anos 2020.
+A media de percentual de uso apresentou uma queda constante de 0,188 nos anos 1990 para 0,178 nos anos 2020.
+A media de percentual de arremesso verdadeiro também aumentou gradualmente de 0,493 nos anos 1990 para 0,541 nos anos 2020.
 */
 
 -- Identificar quais equipes produzem consistentemente jogadores de alto desempenho.
@@ -552,7 +568,7 @@ GROUP BY
 	team
 ORDER BY 
 	num DESC;
--- MIN (12) e MIN (13) produziram vários dos melhores reboteiros ao longo dos anos.
+-- DET (12) e MIN (13) produziram vários dos melhores reboteiros ao longo dos anos.
 
 -- Melhores armadores/criadores de jogadas
 -- Quais equipes produziram os melhores jogadores em assistências por jogo em cada temporada?
@@ -578,7 +594,7 @@ GROUP BY
 	team
 ORDER BY 
 	num DESC;
--- PHX lidera isoladamente com 19 jogadores liderando assistências por jogo ao longo dos anos. Seguido por WAS com 12.
+-- PHX lidera isoladamente com 19 jogadores liderando assistências por jogo ao longo dos anos. Seguido por WAS com 11.
 
 -- Quais equipes produziram os jogadores com melhor classificacao_liquida em cada temporada?
 SELECT
@@ -603,7 +619,7 @@ GROUP BY
 	team
 ORDER BY 
 	num DESC;
--- SAS e GSW lideram com 14 e 13  jogadores ao longo das temporadas.
+-- CHI e GSW lideram com 10  jogadores ao longo das temporadas.
 
 -- Quais equipes produziram os melhores reboteiros ofensivos em cada temporada?
 SELECT
@@ -628,7 +644,7 @@ GROUP BY
 	team
 ORDER BY 
 	num DESC;
--- DET, NYK e MEM possuem respectivamente 10, 9 e 8 jogadores entre os top 5 ao longo dos anos.
+-- MEM, NYK, DEN possuem respectivamente 10, 9 e 8 jogadores entre os top 5 ao longo dos anos.
 
 -- Quais equipes produziram os melhores reboteiros defensivos em cada temporada?
 SELECT
@@ -653,9 +669,9 @@ GROUP BY
 	team
 ORDER BY 
 	num DESC;
--- ORL, CLE, DEN e POR lideram com 10, 9, 8 e 8 respectivamente.
+-- DEN, CLE, LAC e DET lideram com 10, 8, 7 e 7 respectivamente.
 
--- Quais equipes produziram os jogadores com maior uso% em cada temporada?
+-- Quais equipes produziram os jogadores com maior usg_pct em cada temporada?
 SELECT
 	temporada,
 	team,
@@ -678,7 +694,7 @@ GROUP BY
 	team
 ORDER BY 
 	num DESC;
--- LAL (18) e PHI (13) lideram. 
+-- LAL (19) e PHI (11) lideram. 
 
 -- Quais equipes produziram os jogadores com maior percentual_arremesso_verdadeiro em cada temporada?
 SELECT
@@ -703,7 +719,7 @@ GROUP BY
 	team
 ORDER BY 
 	num DESC;
--- BOS (11) e NYK (10) produziram os melhores percentual_arremesso_verdadeiro ao longo dos anos.
+-- BOS (9) e POR (7) produziram os melhores percentual_arremesso_verdadeiro ao longo dos anos.
 
 -- Quais equipes produziram os jogadores com maior percentual_assistencias em cada temporada?
 SELECT
@@ -729,7 +745,7 @@ GROUP BY
 	team
 ORDER BY 
 	num DESC;
--- WAS (13), PHX(13) e UTA possuem 10 jogadores entre os top 5 ao longo dos anos.
+-- UTA (11), WAS(10) e PHX possuem 10 jogadores entre os top 5 ao longo dos anos.
 
 -- Comparar novatos e veteranos — como suas contribuições diferem?
 /*
@@ -811,13 +827,13 @@ GROUP BY
 -- Isso não é surpreendente, já que veteranos possuem mais experiência que novatos.
 
 /*
-MVP 
+MVP & Dream Team
 
 * Usar um índice ponderado (ex.: 40% pontos, 30% rebotes/assistências, 30% eficiência) para encontrar o MVP de uma temporada.
 
 * Montar o quinteto ideal (PG, SG, SF, PF, C) usando estatísticas de todas as temporadas.
 
-
+* Bônus: Comparar sua escolha de MVP com o MVP oficial da NBA naquela temporada.
   */
 
 -- Usar um índice ponderado (ex.: 40% pontos, 30% rebotes/assistências, 30% eficiência) para encontrar o MVP de uma temporada.
@@ -870,4 +886,5 @@ GROUP BY
 ORDER BY 
 	vezes_vencidas DESC;
 -- LeBron James venceu 4 MVPs e pode ser considerado o GOAT no período analisado.
+
 
